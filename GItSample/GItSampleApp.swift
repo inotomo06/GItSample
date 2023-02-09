a6c6afb805c2deddb484c95ae5944620461e1f5b
inotomo06 <21cm0106@jec.ac.jp>
2023-02-09 15:08:08 +0900

    Initial Commit

---
+++ GItSample/GItSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GItSampleApp.swift
+//  GItSample
+//
+//  Created by 井上智輝 on 2023-02-09.
+//
+
+import SwiftUI
+
+@main
+struct GItSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
